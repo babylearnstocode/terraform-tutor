597ef61d200a17e010520f9c0cbd2c923afbd034
Huy Anh Dang <huyanh2504@gmail.com>
2026-05-06 23:46:41 +0200

    deploy first resource

---
+++ main.tf
@@ -0,0 +1,16 @@
+data "aws_ssm_parameter" "amazon_linux" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "test" {
+  ami           = data.aws_ssm_parameter.amazon_linux.value
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "my-free-tier-instance"
+  }
+}
