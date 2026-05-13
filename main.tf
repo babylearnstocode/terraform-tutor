ecfe3b3d91a4da1849d7e8bdc08d5fa4402e25f9
Huy Anh Dang <huyanh2504@gmail.com>
2026-05-14 00:06:42 +0200

    feat: use remote module

--- main.tf
+++ main.tf
@@ -6,11 +6,10 @@ locals {
   ami_id = var.ami_id != null ? var.ami_id : data.aws_ssm_parameter.amazon_linux.value
 }
 
-resource "aws_instance" "test" {
-  instance_type = "t3.micro"
-  ami           = local.ami_id
+module "ec2_instance" {
+  source        = "git::https://github.com/babylearnstocode/terraform-tutor-modules.git//ec2?ref=v0.0.2"
+  instance_type = var.instance_type
+  ami_id        = local.ami_id
+  ec2_tag_name  = var.ec2_tag_name
 
-  tags = {
-    Name = var.ec2_tag_name
-  }
 }
